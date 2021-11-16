3066a5a6111a6937c9e457fb2d23d9be1ef50b0c
友人Ｓ <projectcsj@gmail.com>
2021-11-16 14:04:50 +0900

    Switch ping to utils

--- commands/utils/ping.js
+++ commands/utils/ping.js
@@ -12,8 +12,8 @@ module.exports = {
 		const ping = new MessageEmbed()
 			.setColor('RANDOM')
 			.setAuthor('G Cat Beta Version', 'https://cdn.discordapp.com/avatars/882519953100656680/dd87a83415c4f4b77ade768d34e694f4.png', 'https://csj.yeyunstudio.com')
-			.setTitle('Pong!')
-			.setDescription(`延遲${Math.abs(Date.now() - interaction.createdTimestamp)}ms.`)
+			.setTitle('🏓Pong!')
+			.setDescription(`🕒延遲${Math.abs(Date.now() - interaction.createdTimestamp)}ms.`)
 			.setFooter('Copyright © Project CSJ', 'https://cdn.discordapp.com/avatars/882519953100656680/dd87a83415c4f4b77ade768d34e694f4.png');
 		interaction.reply({ embeds: [ping] });
 	},
